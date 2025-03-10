30b9918260e278fec43435b5730a7c0e642f74cc
Bagg <Cameronwatkins46@gmail.com>
2025-03-10 12:14:18 -0500

    homepage text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Header from './header'
+import skinCare from '../assets/Sophisticated skincare.png'
 
 const Home = () => {
   return (
+    <>
     <Header />
+    <div className='w-full h-screen flex justify-center items-center'>
+        <div className='max-w-1/2 h-5 flex flex-col justify-center items-center'>
+            <img src={skinCare} alt="" />
+        </div>
+    </div>
+    
+    </>
+    
     
   )
 }
