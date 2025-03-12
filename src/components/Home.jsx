b133bb8f98487ba7d7940e01e3e476cd6305e730
Bagg <Cameronwatkins46@gmail.com>
2025-03-12 15:41:27 -0500

    bottom section of homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,40 @@
 import React from "react";
 import Header from "./header";
-import skinCare from "../assets/Sophisticated skincare.png";
+// import skinCare from "../assets/Sophisticated skincare.png";
 import leftArrow from "../assets/buttin-icon-shrunk.png";
 import rightArrow from "../assets/buttin-icon-shrunk-2.png";
+import leftRectangle from "../assets/Rectangle 2779.png";
+import rightRectangle from "../assets/Rectangle 2778.png";
 
 const Home = () => {
   return (
     <>
       <Header />
       <section className="w-full h-screen flex items-center ">
-        <div className="w-full h-5 flex justify-center items-center space-x-80">
+        <div className="w-full h-5 flex justify-center items-center space-x-96">
           <div className="flex items-center gap-3 uppercase text-sm">
             <img className="" src={leftArrow} alt="" />
             <p>Discover A.I.</p>
+            <img className="absolute left-0" src={leftRectangle} alt="" />
           </div>
 
-          <div className="">
-            <img src={skinCare} alt="" />
+          <div className="flex flex-col justify-center items-center font-light text-8xl">
+            <p className="">Sophisticated</p>
+            <p>skincare</p>
           </div>
 
           <div className="flex items-center gap-3 uppercase text-sm">
+            <img className="absolute right-0" src={rightRectangle} alt="" />
             <p>Take Test</p>
             <img src={rightArrow} alt="" />
           </div>
+        </div>
+
+        <div className="w-[25%] flex items-center absolute bottom-0 left-0 uppercase text-sm px-7">
+          <p>
+            Skinstric developed an A.I. that creates a highly-personalised
+            routine tailored to what your skin needs.
+          </p>
         </div>
       </section>
     </>
