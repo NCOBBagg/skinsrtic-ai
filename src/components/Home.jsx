291f535f1fb0ebef429cc018c712ff72841470cf
Bagg <Cameronwatkins46@gmail.com>
2025-03-14 19:24:00 -0500

    fix css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,12 +10,20 @@ const Home = () => {
   return (
     <>
       <Header />
-      <section className="w-full h-screen flex items-center ">
-        <div className="w-full h-5 flex justify-center items-center space-x-96">
-          <div className="flex items-center gap-3 uppercase text-sm">
-            <img className="" src={leftArrow} alt="" />
-            <p>Discover A.I.</p>
-            <img className="absolute left-0" src={leftRectangle} alt="" />
+      <section className="w-full sm:py-32 py-20 sm:px-5 px-5 h-screen flex items-center ">
+        <div className="w-full flex justify-center items-center space-x-96 ">
+          <div
+            id="home-btn"
+            className="flex items-center gap-3 uppercase text-sm"
+          >
+            <img className="absolute left-3 " src={leftArrow} alt="" />
+            <div className="flex justify-center items-center">
+              <p className="">Discover A.I.</p>
+            </div>
+
+            <div className=" flex justify-center items-center">
+              <img className="absolute left" src={leftRectangle} alt="" />
+            </div>
           </div>
 
           <div className="flex flex-col justify-center items-center font-light text-8xl">
@@ -23,14 +31,18 @@ const Home = () => {
             <p>skincare</p>
           </div>
 
-          <div className="flex items-center gap-3 uppercase text-sm">
-            <img className="absolute right-0" src={rightRectangle} alt="" />
+          <div id="home-btn" className="flex items-center gap-2 uppercase text-sm">
+            <img
+              className="absolute right-2 ml-3"
+              src={rightRectangle}
+              alt=""
+            />
             <p>Take Test</p>
             <img src={rightArrow} alt="" />
           </div>
         </div>
 
-        <div className="w-[25%] flex items-center absolute bottom-0 left-0 uppercase text-sm px-7">
+        <div className="w-[25%] flex items-center absolute bottom-0 left-0 uppercase text-sm px-7 ">
           <p>
             Skinstric developed an A.I. that creates a highly-personalised
             routine tailored to what your skin needs.
